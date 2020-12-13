1be56875e64a3ff2d78ad2035caf38ef41b4d9ac
ritika <ritika.awasthy@gmail.com>
2020-12-13 23:09:15 +0530

    added button

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate= document.querySelector("#btn-translate")
+console.log(btnTranslate)
+
+btnTranslate.addEventListener("click",function clickEventHandler()
+{ console.log("Clicked!")
+})
