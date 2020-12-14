bab93af8bd692cb1c874b4e06917be4801601073
ritika <ritika.awasthy@gmail.com>
2020-12-14 13:45:51 +0530

    added app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var btnTranslate= document.querySelector("#btn-translate")
+var txtArea= document.querySelector('#txt-area')
+console.log(txtArea)
 console.log(btnTranslate)
 
 btnTranslate.addEventListener("click",function clickEventHandler()
 { console.log("Clicked!")
+{ console.log("clicked!")
+  console.log("input", txtArea.value)
 })
