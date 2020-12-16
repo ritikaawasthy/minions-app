f8b6c4d3a7952c3004a95cbdd1ce5e1707a6a3d0
ritika <ritika.awasthy@gmail.com>
2020-12-17 03:54:57 +0530

    added minion API

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var btnTranslate= document.querySelector("#btn-translate")
 var txtArea= document.querySelector('#txt-area')
 var output= document.querySelector("#output")
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
 function getTranslationURL(text){
   return serverURL +"?"+"text="+ text
 }
 
+function errorHandler(error){
+  console.log("error occured",error)
+  alert("something wrong with server! please try again later")
+}
 
 btnTranslate.addEventListener("click",function clickEventHandler()
 {
@@ -15,5 +20,9 @@ var inputText = txtArea.value;
 
 fetch(getTranslationURL(inputText))
   .then(response=> response.json())
-  .then(json=>console.log(json.contents.translated))
+  .then(json=>{
+    var translatedText = json.contents.translated;
+    output.innerText = translatedText;
+  })
+  .catch(errorHandler)
 })
