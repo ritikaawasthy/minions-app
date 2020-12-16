f3b00e13fc51da58da9ae2158695551f51576212
ritika <ritika.awasthy@gmail.com>
2020-12-17 03:43:41 +0530

    added error handler

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var btnTranslate= document.querySelector("#btn-translate")
 var txtArea= document.querySelector('#txt-area')
 var output= document.querySelector("#output")
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function getTranslationURL(text){
+  return serverURL +"?"+"text="+ text
+}
+
 
 btnTranslate.addEventListener("click",function clickEventHandler()
 {
-  output.innerText = "ajajasjsjajjsj" + txtArea.value;
+//  output.innerText = "ajajasjsjajjsj" + txtArea.value;
+var inputText = txtArea.value;
+
+fetch(getTranslationURL(inputText))
+  .then(response=> response.json())
+  .then(json=>console.log(json.contents.translated))
 })
